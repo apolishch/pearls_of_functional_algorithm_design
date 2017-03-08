5af0376335fc1b1f02203bbe2d2fca1c40f4bc1c
abraham polishchuk <apolishc@gmail.com>
2017-03-08 02:40:09 +0000

    finished chapter 1

--- freeNumber/src/Lib.hs
+++ freeNumber/src/Lib.hs
@@ -4,6 +4,7 @@ module Lib
 
 import Data.List
 import Data.Array
+import Data.Array.ST
 
 readList :: IO [Int]
 readList = do
@@ -42,14 +43,26 @@ checklist :: [Int] -> Array Int Bool
 checklist xs = accumArray (||) False (0, n) (zip (filter (<= n) xs) (repeat True))
                where n = length xs
 
-
-prosaicChecklist = putStrLn "prosaic goes here"                     
+--runSTArray :: Ix i => (forall s. GHC.ST.ST s (STArray s i e)) -> Array i e
+--newArray :: (Ix i, MArray a e m) => (i, i) -> e -> m (a i e)
+--writeArray :: (Ix i, MArray a e m) => a i e -> i -> e -> m ()
+--sequence :: (Monad m, Traversable t) => t (m a) -> m (t a)
+-- so: writeArray sets the value in a monadic array from some index to some value, taking an array, a key, and a value
+-- newArray creates a new monadic (stateful or otherwise) array. In this case it creates the range from 0 to the length of xs, and fills it with False.
+-- the list comprehension then assigns Trues to the values that exist in xs (with writeArray)
+-- finally sequence lifts the monad above the traversable, and allowing for easy >>= \->
+prosaicChecklist :: [Int] -> Array Int Bool
+prosaicChecklist xs = runSTArray(do {
+                                    a <- newArray (0, n) False;
+                                    sequence [writeArray a x True | x <- xs, x <= n];
+                                    return a})
+                      where n = length xs
                      
 -- similiar to checklist but generates an indexed list of occurence counts.
 -- the zip zips a list of array members as indices with a list of 1s.
 -- the fact that this is an associative array ensures that repetition is just reapplied addition, thus generating a count of occurences.
 -- in effect the way the array is stored by index is the sort.
--- the reason this does not violate the well known O(nln(n)) result, is that the sort is not, in fact, in place
+-- the reason this does not violate the well known O(nln(n)) result, is that the sort is not, in fact, in place (not comparison based)
 -- Additionally this is only really useful if we have some kind of upper bound known on the dataset
 -- TODO: More generic function that takes bounds
 countlist :: [Int] -> Array Int Int
@@ -64,21 +77,52 @@ printSortedArray :: [Int] -> IO ()
 printSortedArray xs = putStrLn "The sorted array is:" >> putStrLn (show $ sortLinearTime xs)
 
 printMinFreeBetterArray :: [Int] -> IO ()
-printMinFreeBetterArray xs = putStrLn "foo"
+printMinFreeBetterArray xs = putStrLn "The minimum free number is:" >> putStrLn (show $ search $ checklist xs)
 
 countListGeneric :: [Int] -> Int -> Int -> Array Int Int
 countListGeneric xs x y | (x < y) = accumArray (+) 0 (x, y)(zip xs (repeat 1))
                         | otherwise = accumArray (+) 0 (y, x)(zip xs (repeat 1))
 
+-- This is awesome because it is linear time, however it has the following drawbacks:
+-- 1) It is not in place
+-- 2) its actually linear on the delta between the bounds. It's only good in the following case: 1) The bounds are reasonably tight, and 2) the array is fairly dense. If there are many gaps, or the bounds are not tight, O(nlnn) may be < O(m)
 sortGeneric :: [Int] -> Int -> Int -> [Int]
 sortGeneric xs x y = concat [replicate k x | (x, k) <- assocs $ countListGeneric xs x y]
 
+-- A more generic solution will take as input the pivot. If the range and density is well understood, pretty good optimizations could be made.
+-- I've done this once for sort and don't feel like doing it here.
+minFreeDivideAndConquer :: [Int] -> Int
+minFreeDivideAndConquer xs |null ([0..b] \\ us) = head([b..] \\ vs)
+                           |otherwise = head([0..] \\ us)
+                         where (us, vs) = partition (< b) xs
+                               b = quot (length xs) 2
+-- This selection of b only really works if the array has a roughly uniform density so that selection based on length makes sense
+-- This algorithm falls into an infinite loop with duplicates. This does not *necessarily* happen
+-- For example 9 8 1 0 2 7 6 5 6 works fine
+-- 9 8 1 0 2 7 6 5 6 3 3 3 infinitely recurses as does 9 8 1 0 2 7 6 5 6 6 4 3 and 0 0 1. Question: What specific criteria force infinite recursion?
+minFreeDivideAndConquerRecursiveFrom :: Int -> (Int, [Int]) -> Int
+minFreeDivideAndConquerRecursiveFrom a (l, xs) | l == 0             = a
+                                               | m == b - a         = minFreeDivideAndConquerRecursiveFrom b (l - m, vs)
+                                               | otherwise          = minFreeDivideAndConquerRecursiveFrom a (m, us)
+                                             where (us, vs) = partition (<b) xs
+                                                   b        = a + 1 + (quot l 2)
+                                                   m        = length us
+
+minFreeDivideAndConquerRecursive :: [Int] -> Int
+minFreeDivideAndConquerRecursive xs = minFreeDivideAndConquerRecursiveFrom 0 (length xs, xs)
+                               
 printGenericallySortedArray :: [Int] -> IO ()
 printGenericallySortedArray xs = let sortGenericList = sortGeneric xs
                                  in readBound >>= \bound1 -> let sortGenericBounded = sortGenericList bound1
                                                                  in readBound >>= \bound2 -> putStrLn "The sorted array is:" >> putStrLn (show $ sortGenericBounded bound2)
 printMinFreeProsaicArray :: [Int] -> IO()
-printMinFreeProsaicArray xs = putStrLn "TODO"
+printMinFreeProsaicArray xs = putStrLn "The minimum free number is:" >> putStrLn (show $ search $ prosaicChecklist xs)
+
+printMinFreeDivideAndConquerNaive :: [Int] -> IO()
+printMinFreeDivideAndConquerNaive xs = putStrLn "The minimum free number is:" >> putStrLn (show $ minFreeDivideAndConquer xs)
+
+printMinFreeDivideAndConquerRecursive :: [Int] -> IO()
+printMinFreeDivideAndConquerRecursive xs = putStrLn "The minimum free number is:" >> putStrLn (show $ minFreeDivideAndConquerRecursive xs)
 
 someFunc :: IO ()
 someFunc = putStrLn "Please select an implementation. Please enter an integer:" >>
@@ -87,6 +131,8 @@ someFunc = putStrLn "Please select an implementation. Please enter an integer:" 
   putStrLn "3. An unrelated aside: Linear time sorting" >>
   putStrLn "4. An even more unrelated aside: General sorting on bounded input" >>
   putStrLn "5. Back on track. An array based implementation with a 'prosaic' checklist">>
+  putStrLn "6. A divide and conquer naive implementation" >>
+  putStrLn "7. A recursive divide and conquer implementation" >>
     getLine >>= \implementation ->
       let li = length implementation
       in case(li) of
@@ -101,6 +147,10 @@ someFunc = putStrLn "Please select an implementation. Please enter an integer:" 
                  Lib.readList >>= printSortedArray
                4 -> putStrLn "You wish to more generally sort an array" >>
                  Lib.readList >>= printGenericallySortedArray
-               5 -> putStrLn "You selected the array based implementation with prosaic checklist">>
+               5 -> putStrLn "You selected the array based implementation with prosaic checklist" >>
                  Lib.readList >>= printMinFreeProsaicArray
+               6 -> putStrLn "You selected the naive divide and conquer implementation" >>
+                 Lib.readList >>= printMinFreeDivideAndConquerNaive
+               7 -> putStrLn "You selected a recursive divide and conquer implementation" >>
+                  Lib.readList >>= printMinFreeDivideAndConquerRecursive
                _ -> putStrLn "This implementation not yet implemented"
